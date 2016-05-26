adddd5503a1eb0145d71e7cada5eb3d260e3e0a4
justyn <justyntemme@gmail.com>
2016-05-26 14:41:47 -0500

    Fix license issue for send.py

--- send.py
+++ send.py
@@ -3,7 +3,7 @@
 ##  Copyright (C) 2016 Justyn Temme
 ##
 ##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU Affero General Public License as published by
+##  it under the terms of the GNU General Public License as published by
 ##  the Free Software Foundation, version 3 only.
 ##
 ##  This program is distributed in the hope that it will be useful,
@@ -11,7 +11,7 @@
 ##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ##  GNU Affero General Public License for more details.
 ##
-##  You should have received a copy of the GNU Affero General Public License
+##  You should have received a copy of the GNU General Public License
 ##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
