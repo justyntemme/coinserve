0344840ffd1e62ba6cfce293642fb3a7532fc420
Justyn Temme <justyntemme@users.noreply.github.com>
2016-05-26 14:19:39 -0500

    Update send.py

--- send.py
+++ send.py
@@ -1,3 +1,20 @@
+##
+##  coinserve - Socket wrapper for coinfetch api
+##  Copyright (C) 2016 Justyn Temme
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Affero General Public License as published by
+##  the Free Software Foundation, version 3 only.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Affero General Public License for more details.
+##
+##  You should have received a copy of the GNU Affero General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 import socket
 import sys
 
